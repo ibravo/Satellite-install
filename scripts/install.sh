5cb973baa8b4f328ce9c8e1bd0dfc34b990d2741
ibravo <ibravo@hotmail.com>
2014-06-03 23:46:35 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,7 +8,7 @@ yum -y localinstall http://repos.fedorapeople.org/repos/mcpierce/qpid-cpp/epel-6
 cp scl.repo /etc/yum.repos.d/scl.repo
 yum -y localinstall http://yum.theforeman.org/nightly/el6/x86_64/foreman-release.rpm
 yum -y install foreman-selinux
-./setup.rb centos6
+./setup.rb centos6 --skip-installer
 service iptables stop
 
 katello-installer --foreman-authentication=true --capsule-tftp=true --capsule-tftp-servername="192.168.2.150" --capsule-dhcp=true --capsule-dhcp-gateway="10.10.10.10" --capsule-dhcp-interface="eth1" --capsule-dhcp-range="10.10.10.20 10.10.10.200" --capsule-dns=true --capsule-dns-forwarders "8.8.8.8" --capsule-dns-interface="eth1" --capsule-dns-zone "hq.ltg" 
