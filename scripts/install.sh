f0dbab3801aabeda04b0323295afbc39a528538d
ibravo <ibravo@hotmail.com>
2014-06-03 23:34:54 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,7 @@ yum -y install foreman-selinux
 ./setup.rb centos6
 service iptables stop
 
-katello-installer --foreman-authentication=true --capsule-tftp=true --capsule-tftp-servername="192.168.2.150" --capsule-dhcp=true --capsule-dhcp-gateway="10.10.10.10" --capsule-dhcp-interface="eth1" --capsule-dhcp-range="10.10.10.20 10.10.10.200" --capsule-dns=true --capsule-dns-forwarders "8.8.8.8" --capsule-dns-interface="eth1"
+katello-installer --foreman-authentication=true --capsule-tftp=true --capsule-tftp-servername="192.168.2.150" --capsule-dhcp=true --capsule-dhcp-gateway="10.10.10.10" --capsule-dhcp-interface="eth1" --capsule-dhcp-range="10.10.10.20 10.10.10.200" --capsule-dns=true --capsule-dns-forwarders "8.8.8.8" --capsule-dns-interface="eth1" --capsule-dns-zone "hq.ltg" 
 # katello-installer --foreman-authentication=true --capsule-tftp=true --capsule-tftp-servername="192.168.2.150" --capsule-dhcp=true --capsule-dhcp-gateway="10.10.10.10" --capsule-dhcp-interface="eth1" --capsule-dhcp-range="10.10.10.20 10.10.10.200" --capsule-dns=true --capsule-dns-forwarders "8.8.8.8" --capsule-dns-interface="eth1" --capsule-dns-reverse="10.10.10.in-addr.arpa" -v
 # katello-installer --foreman-authentication=true --capsule-tftp=true --capsule-tftp-servername="192.168.2.150" --capsule-dhcp=true --capsule-dhcp-gateway="10.10.10.10" --capsule-dhcp-interface="eth1" --capsule-dhcp-range="10.10.10.20 10.10.10.200" -v
  
