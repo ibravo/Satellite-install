e1b0c8e35b9baadfc6dbce85a50742bd89fdb558
ibravo <ibravo@hotmail.com>
2014-06-03 23:18:54 -0400

    Update install.sh
    Remove DNS script

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,7 +10,8 @@ yum -y localinstall http://yum.theforeman.org/nightly/el6/x86_64/foreman-release
 yum -y install foreman-selinux
 ./setup.rb centos6
 service iptables stop
-katello-installer --foreman-authentication=true --capsule-tftp=true --capsule-tftp-servername="192.168.2.150" --capsule-dhcp=true --capsule-dhcp-gateway="10.10.10.10" --capsule-dhcp-interface="eth1" --capsule-dhcp-range="10.10.10.20 10.10.10.200" --capsule-dns=true --capsule-dns-interface="eth1" --capsule-dns-reverse="10.10.10.in-addr.arpa" -v
+# katello-installer --foreman-authentication=true --capsule-tftp=true --capsule-tftp-servername="192.168.2.150" --capsule-dhcp=true --capsule-dhcp-gateway="10.10.10.10" --capsule-dhcp-interface="eth1" --capsule-dhcp-range="10.10.10.20 10.10.10.200" --capsule-dns=true --capsule-dns-interface="eth1" --capsule-dns-reverse="10.10.10.in-addr.arpa" -v
+katello-installer --foreman-authentication=true --capsule-tftp=true --capsule-tftp-servername="192.168.2.150" --capsule-dhcp=true --capsule-dhcp-gateway="10.10.10.10" --capsule-dhcp-interface="eth1" --capsule-dhcp-range="10.10.10.20 10.10.10.200" -v
  
 ## Pending items
 echo Change SELINUX=permissive with 'vim /etc/selinux/config'
