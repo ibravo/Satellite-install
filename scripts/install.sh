160340e5300972e203800004e44bd744f8235934
Ignacio Bravo <ibravo@hotmail.com>
2014-06-05 10:01:38 -0400

    add logs to script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
 # Start with a fresh install of CentOS
+echo "-------------" >> /root/Sat.install.log
+echo $(date) >> /root/Sat.install.log
+echo "Start Install git" >> /root/Sat.install.log
+
 yum install -y git ruby rubygems
 git clone https://github.com/Katello/katello-deploy.git
 cd katello-deploy
+
+echo $(date) >> /root/Sat.install.log
+echo "Start Install foreman-selinux" >> /root/Sat.install.log
+
 yum -y localinstall http://repos.fedorapeople.org/repos/mcpierce/qpid-cpp/epel-6/noarch/qpid-cpp-release-6-1.el6.noarch.rpm
 cp scl.repo /etc/yum.repos.d/scl.repo
 yum -y localinstall http://yum.theforeman.org/nightly/el6/x86_64/foreman-release.rpm
 yum -y install foreman-selinux
 ./setup.rb centos6 --skip-installer
+
+echo $(date) >> /root/Sat.install.log
+echo "Start Katello Installer" >> /root/Sat.install.log
+
 service iptables stop
 
 katello-installer --foreman-authentication=true --capsule-tftp=true --capsule-tftp-servername="192.168.2.150" --capsule-dhcp=true --capsule-dhcp-gateway="10.10.10.10" --capsule-dhcp-interface="eth1" --capsule-dhcp-range="10.10.10.20 10.10.10.200" --capsule-dns=true --capsule-dns-forwarders "10.10.10.10" --capsule-dns-interface="eth1" --capsule-dns-zone "hq.ltg" 
  
 yum -y downgrade puppet-3.5.1 puppet-server-3.5.1
 /etc/init.d/foreman-proxy restart
+
+echo $(date) >> /root/Sat.install.log
+echo "Finish Software Install" >> /root/Sat.install.log
 
 ## Pending items
 echo Change SELINUX=permissive with 'vim /etc/selinux/config'
