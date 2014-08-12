9c53ab1e8b935de5c00f83c6d5057e6caccaaa64
Ignacio Bravo <ibravo@hotmail.com>
2014-08-12 16:31:46 -0400

    added mongo repo

--- scripts/1.install_software.sh
+++ scripts/1.install_software.sh
@@ -25,14 +25,15 @@
 echo "-------------" >> /root/Sat.install.log
 echo "$(date) Update CentOS" >> /root/Sat.install.log
 
+# Install mongodb repo
+#cp ./../configs/yum.repos.d/mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp ./../configs/yum.repos.d/mongodb.repo /etc/yum.repos.d/mongodb.repo
+
 yum -y update
 
 echo "$(date) Start Install git" >> /root/Sat.install.log
 
 yum install -y git ruby rubygems
-
-# Install mongodb repo
-cp ./../configs/yum.repos.d/mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 git clone https://github.com/Katello/katello-deploy.git
 cd katello-deploy
