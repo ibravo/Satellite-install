57be6d67ce6dd67441dc7ca74bae37c1f0223b21
Ignacio Bravo <ibravo@hotmail.com>
2014-08-29 00:28:12 -0400

    added saypuft details

--- scripts/zz.Install.Staypuft.sh
+++ scripts/zz.Install.Staypuft.sh
@@ -1,5 +1,4 @@
 
-
 ==Install==
 yum install -y http://yum.theforeman.org/nightly/el6/x86_64/foreman-release.rpm
 yum -y localinstall http://mirror.pnl.gov/epel/6/x86_64/epel-release-6-8.noarch.rpm
@@ -9,9 +8,46 @@ yum -y install foreman-installer-staypuft
 # echo /usr/share/foreman-installer/modules/foreman/manifests/remote_file.pp:6
 mkdir /var/lib/tftpboot
 mkdir /var/lib/tftpboot/boot
-# echo now modify the file: /opt/rh/ruby193/root/usr/share/gems/gems/staypuft-0.1.20/app/models/staypuft/deployment.rb
+yum -y update
+# echo modify iptables
+cat /proc/sys/net/ipv4/ip_forward
+# change net.ipv4.ip_forward from 0 to 1
+vi /etc/sysctl.conf
+sysctl -e -p /etc/sysctl.conf
 
-staypuft-installer --foreman-configure-epel-repo=true --foreman-plugin-discovery-install-images=true 
+vi /etc/hosts
+192.168.2.151	foreman.hq.ltg	foreman
+
+staypuft-installer --foreman-configure-epel-repo=true --foreman-plugin-discovery-install-images=true --foreman-admin-password="changeme"
+
+iptables -A FORWARD -i eth0 -j ACCEPT
+iptables -A FORWARD -o eth0 -j ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+service iptables save
+vi /etc/sysconfig/iptables
+service iptables restart
+
+Update Centos7 mirror to 
+http://mirror.centos.org/centos/7/os/$arch
+
+Modify /etc/sysconfig/named and add OPTION statement to force IPV4 instead of IPV6 of Centos7
+OPTION="-4"
+
+===
+Openstack Default
+
+ #Dynamic
+cat <<EOF > /tmp/diskpart.cfg
+zerombr
+clearpart --all --initlabel
+part /boot --fstype ext3 --size=500 --ondisk=sda
+part swap --size=1024 --ondisk=sda
+part pv.01 --size=1024 --grow --ondisk=sda
+volgroup vg_root pv.01
+logvol  /  --vgname=vg_root  --size=1 --grow --name=lv_root
+EOF
+
+
 
 
 ==============
