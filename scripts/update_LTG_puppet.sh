17945d299c690228c453b80701fe92b6efa2fadc
Ignacio Bravo <ibravo@hotmail.com>
2015-03-25 18:03:14 -0400

    added new modueles

--- scripts/update_LTG_puppet.sh
+++ scripts/update_LTG_puppet.sh
@@ -2,17 +2,25 @@ echo "Cloning repositories"
 git clone https://github.com/ibravo/astapor.git
 git clone https://github.com/ibravo/ltgcloud.git
 git clone https://github.com/ibravo/puppet-ceph.git
+git clone --recursive https://github.com/redhat-openstack/openstack-puppet-modules
 
 echo "Copying from repo to foreman directory"
-cp -a ./astapor/puppet/modules/* /etc/puppet/environments/production/modules/
-cp -a ./ltgcloud/modules/* /etc/puppet/environments/production/modules/
+mkdir modules
+mkdir modules/ceph
 
-cp -a ./puppet-ceph/manifests/* /etc/puppet/environments/production/modules/puppet-ceph/
+mv astapor/puppet/modules/* modules
+mv openstack-puppet-modules/* modules
+mv puppet-ceph/* modules/ceph
+mv ltgcloud/modules/* modules
+
+\cp -a ./modules/* /etc/puppet/environments/production/modules/
 
 echo "removing temp files"
 rm -rf astapor
+rm -rf openstack-puppet-modules
 rm -rf ltgcloud
 rm -rf puppet-ceph
+rm -rf modules
 
 echo "Now you should refresh the available modules in formena or use Hammer to do so"
 
